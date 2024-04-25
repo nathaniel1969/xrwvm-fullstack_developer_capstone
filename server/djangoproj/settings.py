162d7dc9afe7b292d61461e1f4318388b1c1ac57
nathaniel1969 <56740587+nathaniel1969@users.noreply.github.com>
2024-04-25 13:58:44 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', 'https://nathanielr-8085.theiadockernext-0-labs" +
-    "-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'localhost', 'https://nathanielr-8085.theiadockernext-0-labs' +
+    '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://nathanielr-8085.theiadockernext-0-labs-prod-theiak8s" +
+    'https://nathanielr-8085.theiadockernext-0-labs-prod-theiak8s' +
     '-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
