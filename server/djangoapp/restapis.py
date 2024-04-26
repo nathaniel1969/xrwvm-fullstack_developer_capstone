66a0594284cd28f3d9256f68754804f42dcfd5fa
nathaniel1969 <56740587+nathaniel1969@users.noreply.github.com>
2024-04-26 09:04:53 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url',
-                                   default="http://localhost:5050/")
+backend_url = os.getenv(
+  'backend_url', default="http://localhost:3030")
+sentiment_analyzer_url = os.getenv(
+  'sentiment_analyzer_url',
+  default="http://localhost:5050/")
 
 
 # def get_request(endpoint, **kwargs):
