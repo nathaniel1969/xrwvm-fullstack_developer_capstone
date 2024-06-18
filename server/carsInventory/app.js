d615c0e749d1a63d79f08dc504c1fff38ff7103d
nathaniel1969 <nthaniel_r@hotmail.com>
2024-06-18 12:38:52 -0400

    last commit

--- server/carsInventory/app.js
+++ server/carsInventory/app.js
@@ -9,11 +9,11 @@ const app = express();
 const port = 3050;
 
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const carsData = JSON.parse(fs.readFileSync('car_records.json', 'utf8'));
 
-mongoose.connect('mongodb://mongo_db:27017/', {dbName: 'dealershipsDB'});
+mongoose.connect('mongodb://mongo_db:27017/', { dbName: 'dealershipsDB' });
 
 
 const Cars = require('./inventory');
@@ -26,35 +26,38 @@ try {
 } catch (error) {
     console.error(error);
     // Handle errors properly here
-}app.get('/', async (req, res) => {
+}
+
+app.get('/', async (req, res) => {
     res.send('Welcome to the Mongoose API');
 });
 
 
+
 app.get('/cars/:id', async (req, res) => {
     try {
-        const documents = await Cars.find({dealer_id: req.params.id});
+        const documents = await Cars.find({ dealer_id: req.params.id });
         res.json(documents);
     } catch (error) {
-        res.status(500).json({error: 'Error fetching reviews'});
+        res.status(500).json({ error: 'Error fetching reviews' });
     }
 });
 
 app.get('/carsbymake/:id/:make', async (req, res) => {
     try {
-        const documents = await Cars.find({dealer_id: req.params.id, make: req.params.make});
+        const documents = await Cars.find({ dealer_id: req.params.id, make: req.params.make });
         res.json(documents);
     } catch (error) {
-        res.status(500).json({error: 'Error fetching reviews by car make and model'});
+        res.status(500).json({ error: 'Error fetching reviews by car make and model' });
     }
 });
 
 app.get('/carsbymodel/:id/:model', async (req, res) => {
     try {
-        const documents = await Cars.find({dealer_id: req.params.id, model: req.params.model});
+        const documents = await Cars.find({ dealer_id: req.params.id, model: req.params.model });
         res.json(documents);
     } catch (error) {
-        res.status(500).json({error: 'Error fetching dealers by ID'});
+        res.status(500).json({ error: 'Error fetching dealers by ID' });
     }
 });
 
@@ -63,42 +66,27 @@ app.get('/carsbymaxmileage/:id/:mileage', async (req, res) => {
     try {
         let mileage = parseInt(req.params.mileage);
         if (isNaN(mileage)) {
-            return res.status(400).json({error: 'Invalid mileage parameter'});
+            return res.status(400).json({ error: 'Invalid mileage parameter' });
         }
 
         let condition = {};
-
         if (mileage <= 50000) {
-            condition = {
-                $lte: 50000
-            };
-        } else if (mileage > 50000 && mileage <= 100000) {
-            condition = {
-                $gt: 50000,
-                $lte: 100000
-            };
-        } else if (mileage > 100000 && mileage <= 150000) {
-            condition = {
-                $gt: 100000,
-                $lte: 150000
-            };
-        } else if (mileage > 150000 && mileage <= 200000) {
-            condition = {
-                $gt: 150000,
-                $lte: 200000
-            };
-        } else if (mileage > 200000) {
-            condition = {
-                $gt: 200000
-            };
+            condition = { $lte: mileage };
+        } else if (mileage <= 100000) {
+            condition = { $gt: 50000, $lte: mileage };
+        } else if (mileage <= 150000) {
+            condition = { $gt: 100000, $lte: mileage };
+        } else if (mileage <= 200000) {
+            condition = { $gt: 150000, $lte: mileage };
+        } else {
+            condition = { $gt: 200000 };
         }
 
-        const documents = await Cars.find({dealer_id: req.params.id, mileage: condition});
-
+        const documents = await Cars.find({ dealer_id: req.params.id, mileage: condition });
         res.json(documents);
     } catch (error) {
-        console.error('Error fetching cars by mileage:', error);
-        res.status(500).json({error: 'Error fetching cars by mileage'});
+        console.error('Error fetching cars by max mileage:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
@@ -111,43 +99,35 @@ app.get('/carsbyprice/:id/:price', async (req, res) => {
         }
 
         let condition = {};
-
         if (price <= 20000) {
-            condition = { $lte: 20000 };
-        } else if (price > 20000 && price <= 40000) {
-            condition = { $gt: 20000, $lte: 40000 };
-        } else if (price > 40000 && price <= 60000) {
-            condition = { $gt: 40000, $lte: 60000 };
-        } else if (price > 60000 && price <= 80000) {
-            condition = { $gt: 60000, $lte: 80000 };
-        } else if (price > 80000) {
+            condition = { $lte: price };
+        } else if (price <= 40000) {
+            condition = { $gt: 20000, $lte: price };
+        } else if (price <= 60000) {
+            condition = { $gt: 40000, $lte: price };
+        } else if (price <= 80000) {
+            condition = { $gt: 60000, $lte: price };
+        } else {
             condition = { $gt: 80000 };
         }
 
-        const documents = await Cars.find({
-            dealer_id: req.params.id,
-            price: condition
-        });
-
+        console.log(`Querying with condition: ${JSON.stringify(condition)}`);
+        const documents = await Cars.find({ dealer_id: req.params.id, price: condition });
         res.json(documents);
     } catch (error) {
         console.error('Error fetching cars by price:', error);
-        res.status(500).json({ error: 'Error fetching cars by price' });
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
 
+
 app.get('/carsbyyear/:id/:year', async (req, res) => {
     try {
-        const documents = await Cars.find({
-            dealer_id: req.params.id,
-            year: {
-                $gte: req.params.year
-            }
-        });
+        const documents = await Cars.find({ dealer_id: req.params.id, year: { $gte: req.params.year } });
         res.json(documents);
     } catch (error) {
-        res.status(500).json({error: 'Error fetching dealers by ID'});
+        res.status(500).json({ error: 'Error fetching dealers by ID' });
     }
 });
 
--- server/frontend/src/components/Dealers/Dealer.jsx
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -9,76 +9,77 @@ import review_icon from "../assets/reviewbutton.png";
 import Header from '../Header/Header';
 
 const Dealer = () => {
-  const [dealer, setDealer] = useState({});
-  const [reviews, setReviews] = useState([]);
-  const [unreviewed, setUnreviewed] = useState(false);
-  const [postReview, setPostReview] = useState(<></>);
+    const [dealer, setDealer] = useState({});
+    const [reviews, setReviews] = useState([]);
+    const [unreviewed, setUnreviewed] = useState(false);
+    const [postReview, setPostReview] = useState(<></>);
 
-  const { id } = useParams();
-  const root_url = window.location.origin;
-  const dealer_url = `${root_url}/djangoapp/dealer/${id}`;
-  const reviews_url = `${root_url}/djangoapp/reviews/dealer/${id}`;
-  const post_review = `${root_url}/postreview/${id}`;
+    const { id } = useParams();
+    const root_url = window.location.origin;
+    const dealer_url = `${root_url}/djangoapp/dealer/${id}`;
+    const reviews_url = `${root_url}/djangoapp/reviews/dealer/${id}`;
+    const post_review = `${root_url}/postreview/${id}`;
 
-  const get_dealer = async () => {
-    const res = await fetch(dealer_url);
-    const retobj = await res.json();
-    
-    if (retobj.status === 200 && retobj.dealer) {
-      setDealer(retobj.dealer);
+    const get_dealer = async () => {
+        const res = await fetch(dealer_url);
+        const retobj = await res.json();
+
+        if (retobj.status === 200 && retobj.dealer) {
+            setDealer(retobj.dealer);
+        }
     }
-  }
 
-  const get_reviews = async () => {
-    const res = await fetch(reviews_url);
-    const retobj = await res.json();
-    
-    if (retobj.status === 200 && retobj.reviews) {
-      if (retobj.reviews.length > 0) {
-        setReviews(retobj.reviews);
-      } else {
-        setUnreviewed(true);
-      }
+    const get_reviews = async () => {
+        const res = await fetch(reviews_url);
+        const retobj = await res.json();
+
+        if (retobj.status === 200 && retobj.reviews) {
+            if (retobj.reviews.length > 0) {
+                setReviews(retobj.reviews);
+            } else {
+                setUnreviewed(true);
+            }
+        }
     }
-  }
 
-  const senti_icon = (sentiment) => {
-    let icon = sentiment === "positive" ? positive_icon : sentiment === "negative" ? negative_icon : neutral_icon;
-    return icon;
-  }
-
-  useEffect(() => {
-    get_dealer();
-    get_reviews();
-    if (sessionStorage.getItem("username")) {
-      setPostReview(<a href={post_review}><img src={review_icon} style={{ width: '10%', marginLeft: '10px', marginTop: '10px' }} alt='Post Review' /></a>);
+    const senti_icon = (sentiment) => {
+        let icon = sentiment === "positive" ? positive_icon : sentiment === "negative" ? negative_icon : neutral_icon;
+        return icon;
     }
-  }, [dealer_url, reviews_url, post_review]);
 
-  return (
-    <div style={{ margin: "20px" }}>
-      <Header />
-      <div style={{ marginTop: "10px" }}>
-        <h1 style={{ color: "grey" }}>{dealer.full_name}{postReview}</h1>
-        <h4 style={{ color: "grey" }}>{dealer.city}, {dealer.address}, Zip - {dealer.zip}, {dealer.state}</h4>
-      </div>
-      <div className="reviews_panel">
-        {reviews.length === 0 && !unreviewed ? (
-          <div>Loading Reviews....</div>
-        ) : unreviewed ? (
-          <div>No reviews yet!</div>
-        ) : (
-          reviews.map(review => (
-            <div className='review_panel' key={review.id}>
-              <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment' />
-              <div className='review'>{review.review}</div>
-              <div className="reviewer">{review.name} {review.car_make} {review.car_model} {review.car_year}</div>
+    useEffect(() => {
+        get_dealer();
+        get_reviews();
+        if (sessionStorage.getItem("username")) {
+            setPostReview(<a href={post_review}><img src={review_icon} style={{ width: '10%', marginLeft: '10px', marginTop: '10px' }} alt='Post Review' /></a>);
+        }
+    }, [dealer_url, reviews_url, post_review]);
+
+    return (
+        <div style={{ margin: "20px" }}>
+            <Header />
+            <div style={{ marginTop: "10px" }}>
+                <h1 style={{ color: "grey" }}>{dealer.full_name}{postReview}</h1>
+                <h4 style={{ color: "grey" }}>{dealer.city}, {dealer.address}, Zip - {dealer.zip}, {dealer.state}</h4>
+                <h5><a href={`/searchcars/${id}`}>SearchCars</a></h5>
             </div>
-          ))
-        )}
-      </div>
-    </div>
-  )
+            <div className="reviews_panel">
+                {reviews.length === 0 && !unreviewed ? (
+                    <div>Loading Reviews....</div>
+                ) : unreviewed ? (
+                    <div>No reviews yet!</div>
+                ) : (
+                    reviews.map(review => (
+                        <div className='review_panel' key={review.id}>
+                            <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment' />
+                            <div className='review'>{review.review}</div>
+                            <div className="reviewer">{review.name} {review.car_make} {review.car_model} {review.car_year}</div>
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Dealer;
